907de03d0b73080d4464b5eb22188acf7ee9d811
Itz-Agasta <rupamgolui69@gmail.com>
2025-07-06 21:36:06 +0530

    add merge sort

--- sort/normal.cpp
+++ sort/normal.cpp
@@ -61,17 +61,69 @@ void insertion_sort(int arr[], int n)
         }
     }
 }
+
+// Merge Sort
+// Rule: divide & merge
+// low high mid these are indexs
+// TC= O(log2 n)
+// SC= O(n)--> worst
+void merge(vector<int> &arr, int low, int high, int mid)
+{
+    vector<int> temp;
+    int left = low;      // [left.........mid]---> arr1
+    int right = mid + 1; // [right......high]----> arr2
+
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while (right <= high)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    // Merge the two sorted array
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+void merge_sort(vector<int> &arr, int low, int high)
+{
+    if (low == high)
+        return;
+    int mid = (high + low) / 2;
+    merge_sort(arr, low, mid);
+    merge_sort(arr, mid + 1, high);
+    merge(arr, low, high, mid);
+}
+
 int main()
 {
     // Input
     int n;
     cin >> n;
-    int arr[n];
+    vector<int> arr(n);
     for (int i = 0; i < n; i++)
         cin >> arr[i];
 
     // Fn call
-    insertion_sort(arr, n);
+    merge_sort(arr, 0, n - 1);
 
     // Output;
     for (int i = 0; i < n; i++)
